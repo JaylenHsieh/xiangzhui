6aedebb497471d7961c32ae5c5a36f86ee3e72f8
JaylenHsieh <zjjxxiejunliang@gmail.com>
2020-04-22 01:09:35 +0800

    Revert "Revert "一大波完善，既不会全能用了""
    This reverts commit 10b4f03d9fa52cf717294212bee703fff1b81a63.

---
+++ app/src/main/java/com/hsieh/xiangzhui/ui/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.hsieh.xiangzhui.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hsieh.xiangzhui.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
