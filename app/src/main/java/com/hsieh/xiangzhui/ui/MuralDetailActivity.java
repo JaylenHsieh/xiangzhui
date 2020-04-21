95fe0f9ddfbb3adbe101e0a4ff896e00e4153d6f
JaylenHsieh <zjjxxiejunliang@gmail.com>
2020-04-22 01:10:52 +0800

    更正提交日志，一大波完善，可正常演示

--- app/src/main/java/com/hsieh/xiangzhui/ui/MuralDetailActivity.java
+++ app/src/main/java/com/hsieh/xiangzhui/ui/MuralDetailActivity.java
@@ -15,16 +15,19 @@ public class MuralDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mural_detail);
         Intent intent = getIntent();
+
         String muralTheme = intent.getStringExtra("mural_theme");
         String muralClass = intent.getStringExtra("mural_class");
         Float muralScore = intent.getFloatExtra("mural_score",(float) 4.7);
         int muralLength = intent.getIntExtra("mural_length",2510);
         int muralWidth = intent.getIntExtra("mural_width",1210);
+
         TextView tvMuralTheme = findViewById(R.id.tvMuralTheme);
         TextView tvMuralClass = findViewById(R.id.tvMuralClass);
         TextView tvMuralScore = findViewById(R.id.tvMuralScore);
         TextView tvMuralLength = findViewById(R.id.tvLength);
         TextView tvMuralWidth = findViewById(R.id.tvWidth);
+        
         tvMuralTheme.setText(muralTheme);
         tvMuralClass.setText(muralClass);
         tvMuralScore.setText(String.valueOf(muralScore));
